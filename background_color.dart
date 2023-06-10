4ed645a257dc6254a00722c199eddc71a72268b5
CodingWithZohaib <113612574+Zohaib521321@users.noreply.github.com>
2023-06-09 22:22:32 -0700

    Add files via upload

---
+++ background_color.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("circular icon button"),
+        ),
+        body:
+        Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                shape: CircleBorder(), padding: EdgeInsets.all(30)),
+            child: Icon(
+              Icons.add,
+              size: 50,
+            ),
+            onPressed: () {},
+          ),
+        ),
+
+    );
+  }
+
+}
